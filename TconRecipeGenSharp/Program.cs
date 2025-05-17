a82de4359b82d82badcfedf9f2abdcbe5a5c7e01
霧雨烨 <Web_13234@outlook.com>
2025-05-17 14:31:57 +0800

    init

---
+++ TconRecipeGenSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> errors = [];
+
+if (args is not [var path]) errors.Add("Please drag and drop a input file to this program.");
+
+if (errors.Count > 0)
+{
+    Console.WriteLine("Errors:");
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error);
+    }
+    return 0;
+}
+
+Console.WriteLine("Hello, World!");
+return 0;
